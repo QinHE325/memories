9ba9bb62660a057b1d6eb31df8b2d4aeef727fec
Qin He <heqin@usc.edu>
2023-05-07 18:20:11 -0700

    add redux part

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extend: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extend: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://helium_admin:2023NEU123@cluster0.1q8bl.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
