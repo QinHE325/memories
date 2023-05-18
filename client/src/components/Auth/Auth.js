e5788dd9ba00f8c042ef14e0636d2638c57b9bda
Qin He <heqin@usc.edu>
2023-05-17 23:46:27 -0700

    add auth files

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import useStyles from './styles';
 
 export const Auth = () => {
     const state = null;
-    return (
-        <div>Auth</div>
-    )
+    const classes = useStyles();
+    
 }
 
 export default Auth;
