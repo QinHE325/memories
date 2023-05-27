6df8157ce4cb60770ea522d855f055c6b500ebd0
Qin He <heqin@usc.edu>
2023-05-26 19:53:30 -0700

    display like and delete only for authenticated user

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -37,9 +37,11 @@ const Post = ({ post, setCurrentId }) => {
           <Typography variant="h6">{post.name}</Typography>
           <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
         </div>
+        {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
         <div className={classes.overlay2}>
           <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="medium" /></Button>
         </div>
+        )}
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
         </div>
@@ -51,7 +53,9 @@ const Post = ({ post, setCurrentId }) => {
           <Button size="small" color="primary" disabled={!user?.result} onClick={() => {dispatch(likePost(post._id))}}>
             <Likes />
           </Button>
-          <Button size="small" color="primary" onClick={() => {dispatch(deletePost(post._id))}}><DeleteIcon fontSize="small" /> Delete</Button>
+          {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
+            <Button size="small" color="primary" onClick={() => {dispatch(deletePost(post._id))}}><DeleteIcon fontSize="small" /> Delete</Button>
+          )}
         </CardActions>
       </Card>
     )
